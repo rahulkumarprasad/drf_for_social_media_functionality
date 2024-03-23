06e23d0ec382dfa6a77831e60698af58e684943e
rahul <rahulkumarpdd@gmail.com>
2024-03-23 14:48:22 +0530

    updated description

--- api/models.py
+++ api/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
-    """This models is used for storing data related to vendors"""
+    """This models is used for storing data related to User Details"""
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     frend_requests = models.ManyToManyField(User, related_name="frend_requests")
     friends = models.ManyToManyField(User, related_name="list_frends")
